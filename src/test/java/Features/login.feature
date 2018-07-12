e6f2109699136d8317085a2e8f3087cc72fc5d17
saipragna88 <saipragna88@gmail.com>
2018-07-12 14:09:21 -0400

    Data driven BDD with Examples and Scenario Outline keywords

--- src/test/java/Features/login.feature
+++ src/test/java/Features/login.feature
@@ -1,10 +1,14 @@
 Feature: Free CRM login feature
 
-Scenario: Login scenario
+Scenario Outline: Login scenario
 
 Given user already on login page
 When title of login page is Free CRM software in the cloud powers sales and customer service
-Then user enters "tiny06" and "tiny06"
+Then user enters "<username>" and "<password>"
 Then user clicks login button
 Then user is on home page
 
+Examples:
+	|username|password|
+	|tiny06  |tiny06  |
+	|naveenk |test@123|
--- test-output/report.js
+++ test-output/report.js
@@ -6,13 +6,79 @@ formatter.feature({
   "id": "free-crm-login-feature",
   "keyword": "Feature"
 });
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 3,
   "name": "Login scenario",
   "description": "",
   "id": "free-crm-login-feature;login-scenario",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user already on login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM software in the cloud powers sales and customer service",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"\u003cusername\u003e\" and \"\u003cpassword\u003e\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 11,
+  "name": "",
+  "description": "",
+  "id": "free-crm-login-feature;login-scenario;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 12,
+      "id": "free-crm-login-feature;login-scenario;;1"
+    },
+    {
+      "cells": [
+        "tiny06",
+        "tiny06"
+      ],
+      "line": 13,
+      "id": "free-crm-login-feature;login-scenario;;2"
+    },
+    {
+      "cells": [
+        "naveenk",
+        "test@123"
+      ],
+      "line": 14,
+      "id": "free-crm-login-feature;login-scenario;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "Login scenario",
+  "description": "",
+  "id": "free-crm-login-feature;login-scenario;;2",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario Outline"
 });
 formatter.step({
   "line": 5,
@@ -27,6 +93,10 @@ formatter.step({
 formatter.step({
   "line": 7,
   "name": "user enters \"tiny06\" and \"tiny06\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
   "keyword": "Then "
 });
 formatter.step({
@@ -43,14 +113,14 @@ formatter.match({
   "location": "LoginStepDefinitions.user_already_on_login_page()"
 });
 formatter.result({
-  "duration": 11092966968,
+  "duration": 11185293386,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinitions.title_of_login_page_is_Free_CRM_software_in_the_cloud_powers_sales_and_customer_service()"
 });
 formatter.result({
-  "duration": 58800422,
+  "duration": 28209386,
   "status": "passed"
 });
 formatter.match({
@@ -67,21 +137,103 @@ formatter.match({
   "location": "LoginStepDefinitions.user_enters_username_and_password(String,String)"
 });
 formatter.result({
-  "duration": 504597916,
+  "duration": 538669756,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinitions.user_clicks_login_button()"
 });
 formatter.result({
-  "duration": 8840764816,
+  "duration": 4348611668,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinitions.user_is_on_home_page()"
 });
 formatter.result({
-  "duration": 11536428,
+  "duration": 6174565,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "Login scenario",
+  "description": "",
+  "id": "free-crm-login-feature;login-scenario;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 5,
+  "name": "user already on login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM software in the cloud powers sales and customer service",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters \"naveenk\" and \"test@123\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_already_on_login_page()"
+});
+formatter.result({
+  "duration": 11909981054,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.title_of_login_page_is_Free_CRM_software_in_the_cloud_powers_sales_and_customer_service()"
+});
+formatter.result({
+  "duration": 22299345,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "naveenk",
+      "offset": 13
+    },
+    {
+      "val": "test@123",
+      "offset": 27
+    }
+  ],
+  "location": "LoginStepDefinitions.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 1260815752,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_clicks_login_button()"
+});
+formatter.result({
+  "duration": 2328639855,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinitions.user_is_on_home_page()"
+});
+formatter.result({
+  "duration": 9159910,
   "status": "passed"
 });
 });
